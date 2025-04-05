bb85932b5a6c59e6fe8df9da7b054cdad36a5f60
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-04-05 19:30:51 +0200

    done

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Listening on port 4000")
+})
+
+app.get('/', (req, res) => {
+    res.json({ mssg : 'Working with env'})
 })
