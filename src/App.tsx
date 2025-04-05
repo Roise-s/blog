806b9bbb33dc2b9f9a3c87ad0b479fab3025a455
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-04-05 17:23:27 +0200

    new start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
+import './App.css'
+import LoginPage from './pages/login/page'
 
 function App() {
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <LoginPage />
   )
 }
 
